e35a3da1607303d56f50d741cbb75bde3cabf57b
zhangyaochun <zhangyaochun@wandoujia.com>
2014-08-11 14:31:23 +0800

    http methods

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = [
+    'get',
+    'head',
+    'post',
+    'put',
+    'delete',
+    'options',
+    'trace',
+    'copy',
+    'lock',
+    'mkcol',
+    'move',
+    'purge',
+    'propfind',
+    'proppatch',
+    'unlock',
+    'report',
+    'mkactivity',
+    'checkout',
+    'merge',
+    'm-search',
+    'notify',
+    'subscribe',
+    'unsubscribe',
+    'patch',
+    'search'
+];
